c293f9e228112ebd6eee0454e133a804d4fd9f8f
Robert Hu <robert.y.hu.97@gmail.com>
2022-11-14 17:13:56 -0800

    Callback review and promise construction

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,29 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = (filePath, callback) => {
   // TODO
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      var firstLine = data.split('\n')[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = (url, callback) => {
   // TODO
+  request.get(url, (err, response) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, response.statusCode);
+    }
+  });
+
 };
 
 // Export these functions so we can test them and reuse them in later exercises
