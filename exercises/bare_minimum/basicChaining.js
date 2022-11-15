abfd7feba9a608fc128483098d601d618fcc4d84
Robert Hu <robert.y.hu.97@gmail.com>
2022-11-15 10:20:24 -0800

    Basic Chaingin

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -9,12 +9,46 @@
  */
 
 var fs = require('fs');
+// var fs = require('node:fs/promises');
 var Promise = require('bluebird');
 
-
+var Promisification = require('./promisification.js');
+var PromiseConstructor = require('./promiseConstructor.js');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  // PromiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+  //   .then(data => {
+  //     Promisification.getGitHubProfileAsync(data)
+  //       .then(data => {
+  //         data = JSON.stringify(data);
+  //         fs.writeFile(writeFilePath, data)
+  //           .then(data => {
+  //             console.log(data);
+  //           });
+  //       })
+  //       .catch(err => {
+  //         console.log('Failed to GET GitHub user data!');
+  //       });
+  //   })
+  //   .catch(err => {
+  //     console.log(`Failed to open ${readFilePath}!`);
+  //   });
+  return PromiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    .then(line => {
+      if (!line || !line.length) {
+        throw new Error('Invalid GitHub handle read!');
+      } else {
+        return line;
+      }
+    })
+    .then(handle => {
+      return Promisification.getGitHubProfileAsync(handle);
+    })
+    .then(user => {
+      user = JSON.stringify(user);
+      fs.writeFileSync(writeFilePath, user);
+    });
 };
 
 // Export these functions so we can test them
